a4cc779acabb9727a75a062fdffbce6dae8aad3b
baljinder0630 <baljindersingh41256@gmail.com>
2024-11-23 01:29:45 +0530

    Refactor index.js to improve code structure and add rate limiting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import api from './routes/api.js'
-import rateLimit from 'express-rate-limit'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import api from './routes/api.js';
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
@@ -21,18 +21,20 @@ const limiter = rateLimit({
     message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
+// Enable trust proxy
+app.set('trust proxy', 1);
+
 // Apply the rate limiter to all requests
 app.use(limiter);
 
 app.use(cors(corsOptions));
 
-
-app.use('/api', api)
+app.use('/api', api);
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
