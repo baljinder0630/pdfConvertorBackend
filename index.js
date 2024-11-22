6599ea360edeb2712a9e12e1e604a3629b7f37de
baljinder0630 <baljindersingh41256@gmail.com>
2024-11-22 19:48:31 +0530

    Update CORS origin to allow all origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const corsOptions = {
-    origin: 'http://your-frontend-url.com', // Replace with your frontend URL
+    origin: '*',
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type,Authorization',
 };
